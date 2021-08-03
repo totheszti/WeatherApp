c33fb42ff6e4d8a8152ca9f537953e072793f110
Eszter <toth.eszter@lanoga.hu>
2021-08-03 20:38:53 +0200

    Responsive layout

--- src/currentLocation.js
+++ src/currentLocation.js
@@ -5,6 +5,8 @@ import loader from './assets/fidgetspinner.gif'
 import apiKeys from "./apiKeys";
 import classNames from 'classnames';
 import ReactAnimatedWeather from "react-animated-weather";
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 const dateBuilder = (d) => {
     let months = [
@@ -30,6 +32,8 @@ const dateBuilder = (d) => {
         "Péntek",
         "Szombat",
     ];
+
+    let apiKey = "";
 
     let day = days[d.getDay()];
     let date = d.getDate();
@@ -98,6 +102,7 @@ class Weather extends React.Component {
         const api_call = await fetch(
             `${apiKeys.base}weather?lat=${lat}&lon=${lon}&units=metric&&lang=hu&APPID=${apiKeys.key}`
         );
+
         const data = await api_call.json();
         this.setState({
             lat: lat,
@@ -146,86 +151,98 @@ class Weather extends React.Component {
     };
 
     render() {
-        if (!this.state) {
-            return <span>Töltődik...</span>;
-        } else if (this.state.temperatureC) {
+        if(this.apiKey === ""){
             return (
-                <React.Fragment>
-                    <div className={classNames('city', {
-                        'bg-clear': this.state.main === "Clear",
-                        'bg-haze': this.state.main === "Haze",
-                        'bg-clouds': this.state.main === "Clouds",
-                        'bg-rain': this.state.main === "Rain",
-                        'bg-snow': this.state.main === "Snow",
-                        'bg-dust': this.state.main === "Dust",
-                        'bg-drizzle': this.state.main === "Drizzle",
-                        'bg-fog': this.state.main === "Fog",
-                        'bg-smoke': this.state.main === "Smoke",
-                        'bg-tornado': this.state.main === "Tornado"
-                    })}>
-                        <div className="title">
-                            <h2>{this.state.city}</h2>
-                            <h3>{this.state.country}</h3>
-                        </div>
-                        <div className="mb-icon">
-                            {" "}
-                            <ReactAnimatedWeather
-                                icon={this.state.icon}
-                                color={defaults.color}
-                                size={defaults.size}
-                                animate={defaults.animate}
-                            />
-                        </div>
-                        <div className="date-time">
-                            <div className="dmy">
-                                <div id="txt"></div>
-                                <div className="current-time">
-                                    <Clock format="HH:mm:ss" interval={1000} ticking={true}/>
+                <Popup >
+                    <div>Add meg az API kulcsodat!</div>
+                    <form className="form-horizontal">
+                        <input type="text" className="form-control" value={this.apiKey}/>
+                    </form>
+                </Popup>
+            )
+        }
+        else {
+            if (!this.state) {
+                return <span>Töltődik...</span>;
+            } else if (this.state.temperatureC) {
+                return (
+                    <React.Fragment>
+                        <div className={classNames('city', {
+                            'bg-clear': this.state.main === "Clear",
+                            'bg-haze': this.state.main === "Haze",
+                            'bg-clouds': this.state.main === "Clouds",
+                            'bg-rain': this.state.main === "Rain",
+                            'bg-snow': this.state.main === "Snow",
+                            'bg-dust': this.state.main === "Dust",
+                            'bg-drizzle': this.state.main === "Drizzle",
+                            'bg-fog': this.state.main === "Fog",
+                            'bg-smoke': this.state.main === "Smoke",
+                            'bg-tornado': this.state.main === "Tornado"
+                        })}>
+                            <div className="title">
+                                <h2>{this.state.city}</h2>
+                                <h3>{this.state.country}</h3>
+                            </div>
+                            <div className="mb-icon">
+                                {" "}
+                                <ReactAnimatedWeather
+                                    icon={this.state.icon}
+                                    color={defaults.color}
+                                    size={defaults.size}
+                                    animate={defaults.animate}
+                                />
+                            </div>
+                            <div className="date-time">
+                                <div className="dmy">
+                                    <div id="txt"></div>
+                                    <div className="current-time">
+                                        <Clock format="HH:mm:ss" interval={1000} ticking={true}/>
+                                    </div>
+                                    <div className="current-date">{dateBuilder(new Date())}</div>
                                 </div>
-                                <div className="current-date">{dateBuilder(new Date())}</div>
+                                <div className="temperature">
+                                    <p>
+                                        {this.state.temperatureC}°C
+                                    </p>
+                                </div>
+                                <div style={{
+                                    width: "100%",
+                                    float: "right"
+                                }}>
+                                    <div className="w-100">
+                                        <p className="feelsLike">
+                                            Hőérzet: {this.state.feelsLike}°C
+                                        </p>
+                                    </div>
+                                    {/*<div className="w-100">*/}
+                                    {/*    <h3 className="text-white" style={{ float: "right"}}>*/}
+                                    {/*        Páratartalom: {this.state.humidity}%*/}
+                                    {/*    </h3>*/}
+                                    {/*</div>*/}
+                                </div>
                             </div>
-                            <div className="temperature">
-                                <p>
-                                    {this.state.temperatureC}°C
-                                </p>
-                            </div>
-                            <div style={{
-                                width: "100%",
-                                float: "right"
-                            }}>
-                            <div className="w-100">
-                                <p className="text-white" style={{ float: "right"}}>
-                                    Hőérzet: {this.state.feelsLike}°C
-                                </p>
-                            </div>
-                            {/*<div className="w-100">*/}
-                            {/*    <h3 className="text-white" style={{ float: "right"}}>*/}
-                            {/*        Páratartalom: {this.state.humidity}%*/}
-                            {/*    </h3>*/}
-                            {/*</div>*/}
-                            </div>
-                        </div>
 
-                    </div>
-                    <Forecast icon={this.state.icon} weather={this.state.main}/>
-                </React.Fragment>
-            );
-        } else {
-            return (
-                <React.Fragment>
-                    <div style={{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        width: "100%",
-                        height: "100%"
-                    }}>
-                        <h3 style={{color: "white", marginTop: "10px"}}>
-                            A jelenlegi tartózkodási helyed rögtön megjelenik... Kis türelmet
-                        </h3>
-                    </div>
-                </React.Fragment>
-            );
+                        </div>
+                        <Forecast icon={this.state.icon} weather={this.state.main}/>
+                    </React.Fragment>
+                );
+            } else {
+                return (
+                    <React.Fragment>
+                        <div style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            width: "100%",
+                            height: "100%"
+                        }}>
+                            <h3 style={{color: "white", marginTop: "10px"}}>
+                                A jelenlegi tartózkodási helyed rögtön megjelenik... Kis türelmet
+                            </h3>
+                        </div>
+                    </React.Fragment>
+                );
+            }
         }
     }
 }
--- src/forecast.js
+++ src/forecast.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import apiKeys from "./apiKeys";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 function Forecast(props) {
     const [query, setQuery] = useState("");
@@ -22,7 +24,7 @@ function Forecast(props) {
                 console.log(error);
                 setWeather("");
                 setQuery("");
-                setError({ message: "nem található", query: query });
+                setError({message: "nem található", query: query});
             });
     };
 
@@ -43,66 +45,68 @@ function Forecast(props) {
         search("Pécs");
     }, []);
 
-    return (
-        <div className="forecast">
-            <div className="today-weather">
-                <h3>További városok:</h3>
-                <div className="search-box">
-                    <input
-                        type="text"
-                        className="search-bar"
-                        placeholder="Keresés..."
-                        onChange={(e) => setQuery(e.target.value)}
-                        onBlur={query !== "" ? search : ""}
-                        value={query}
-                    />
-                </div>
-                <ul>
-                    {typeof weather.main != "undefined" ? (
-                        <div>
-                            {" "}
-                            <li className="cityHead">
-                                <p>
-                                    {weather.name}, {weather.sys.country}
-                                </p>
-                                <img
-                                    className="temp"
-                                    src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
-                                />
-                            </li>
-                            <li>
-                                Hőmérséklet{" "}
-                                <span className="temp">
+        return (
+            <div className="forecast">
+                <div className="today-weather">
+                    <h3>További városok:</h3>
+                    <div className="search-box">
+                        <input
+                            type="text"
+                            className="search-bar"
+                            placeholder="Keresés..."
+                            onChange={(e) => setQuery(e.target.value)}
+                            onBlur={query !== "" ? search : ""}
+                            value={query}
+                        />
+                        { query === "" ?
+                            <FontAwesomeIcon icon="sync-alt" onClick={search}/> : ""}
+                    </div>
+                    <ul>
+                        {typeof weather.main != "undefined" ? (
+                            <div>
+                                {" "}
+                                <li className="cityHead">
+                                    <p>
+                                        {weather.name}, {weather.sys.country}
+                                    </p>
+                                    <img
+                                        className="temp"
+                                        src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+                                    />
+                                </li>
+                                <li>
+                                    Hőmérséklet{" "}
+                                    <span className="temp">
                   {Math.round(weather.main.temp)}°C ({weather.weather[0].main})
                 </span>
-                            </li>
-                            <li>
-                                Hőérzet{" "}
-                                <span className="temp">
+                                </li>
+                                <li>
+                                    Hőérzet{" "}
+                                    <span className="temp">
                   {Math.round(weather.main.feels_like)}°C
                 </span>
-                            </li>
-                            <li>
-                                Páratartalom{" "}
-                                <span className="temp">
+                                </li>
+                                <li>
+                                    Páratartalom{" "}
+                                    <span className="temp">
                   {Math.round(weather.main.humidity)}%
                 </span>
-                            </li>
-                            <li>
-                                Szél{" "}
-                                <span className="temp">
+                                </li>
+                                <li>
+                                    Szél{" "}
+                                    <span className="temp">
                   {Math.round(weather.wind.speed)} Km/h
                 </span>
+                                </li>
+                            </div>
+                        ) : (
+                            <li>
+                                {error.query} {error.message}
                             </li>
-                        </div>
-                    ) : (
-                        <li>
-                            {error.query} {error.message}
-                        </li>
-                    )}
-                </ul>
+                        )}
+                    </ul>
+                </div>
             </div>
-        </div>
-    );
+        );
 }
 export default Forecast;
